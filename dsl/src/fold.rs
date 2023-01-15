62eb516bcec2ad9e9eca7c6272acaf7215a8edd4
Garret Fick <garret@ficksworkshop.com>
2023-01-15 14:12:40 +0100

    Enable folding to return an error (#25)

--- dsl/src/fold.rs
+++ dsl/src/fold.rs
@@ -14,7 +14,7 @@ use crate::dsl::*;
 // folded version of itself.
 pub(crate) trait Foldable {
     type Mapped;
-    fn fold<F: Fold + ?Sized>(self, folder: &mut F) -> Self::Mapped;
+    fn fold<F: Fold<E> + ?Sized, E>(self, folder: &mut F) -> Result<Self::Mapped, E>;
 }
 
 impl<X> Foldable for Vec<X>
@@ -22,115 +22,114 @@ where
     X: Foldable,
 {
     type Mapped = Vec<X::Mapped>;
-    fn fold<F: Fold + ?Sized>(self, folder: &mut F) -> Self::Mapped {
+    fn fold<F: Fold<E> + ?Sized, E>(self, folder: &mut F) -> Result<Self::Mapped, E> {
         self.into_iter().map(|x| x.fold(folder)).collect()
     }
 }
 
-impl<X> Foldable for Option<X>
-where
-    X: Foldable,
-{
-    type Mapped = Option<X::Mapped>;
-    fn fold<F: Fold + ?Sized>(self, folder: &mut F) -> Self::Mapped {
-        self.map(|x| x.fold(folder))
+pub trait Fold<E> {
+    fn fold(&mut self, node: Library) -> Result<Library, E> {
+        Ok(Library {
+            elems: Foldable::fold(node.elems, self)?,
+        })
     }
-}
-
-pub trait Fold {
-    fn fold(&mut self, node: Library) -> Library {
-        Library {
-            elems: Foldable::fold(node.elems, self),
-        }
-    }
-    fn fold_library_element_declaration(&mut self, node: LibraryElement) -> LibraryElement {
+    fn fold_library_element_declaration(
+        &mut self,
+        node: LibraryElement,
+    ) -> Result<LibraryElement, E> {
         match node {
             LibraryElement::FunctionBlockDeclaration(function_block_decl) => {
-                LibraryElement::FunctionBlockDeclaration(
-                    self.fold_function_block_declaration(function_block_decl),
-                )
+                Ok(LibraryElement::FunctionBlockDeclaration(
+                    self.fold_function_block_declaration(function_block_decl)?,
+                ))
             }
-            LibraryElement::FunctionDeclaration(function_decl) => {
-                LibraryElement::FunctionDeclaration(self.fold_function_declaration(function_decl))
-            }
-            LibraryElement::ProgramDeclaration(program_decl) => {
-                LibraryElement::ProgramDeclaration(self.fold_program_declaration(program_decl))
-            }
-            _ => node,
+            LibraryElement::FunctionDeclaration(function_decl) => Ok(
+                LibraryElement::FunctionDeclaration(self.fold_function_declaration(function_decl)?),
+            ),
+            LibraryElement::ProgramDeclaration(program_decl) => Ok(
+                LibraryElement::ProgramDeclaration(self.fold_program_declaration(program_decl)?),
+            ),
+            _ => Ok(node),
         }
     }
 
     fn fold_function_block_declaration(
         &mut self,
         node: FunctionBlockDeclaration,
-    ) -> FunctionBlockDeclaration {
-        FunctionBlockDeclaration {
+    ) -> Result<FunctionBlockDeclaration, E> {
+        Ok(FunctionBlockDeclaration {
             name: node.name,
-            variables: Foldable::fold(node.variables, self),
+            variables: Foldable::fold(node.variables, self)?,
             body: node.body,
-        }
+        })
     }
 
-    fn fold_function_declaration(&mut self, node: FunctionDeclaration) -> FunctionDeclaration {
-        FunctionDeclaration {
+    fn fold_function_declaration(
+        &mut self,
+        node: FunctionDeclaration,
+    ) -> Result<FunctionDeclaration, E> {
+        Ok(FunctionDeclaration {
             name: node.name.clone(),
             return_type: node.return_type,
-            variables: Foldable::fold(node.variables, self),
+            variables: Foldable::fold(node.variables, self)?,
             body: node.body,
-        }
+        })
     }
 
-    fn fold_program_declaration(&mut self, node: ProgramDeclaration) -> ProgramDeclaration {
-        ProgramDeclaration {
+    fn fold_program_declaration(
+        &mut self,
+        node: ProgramDeclaration,
+    ) -> Result<ProgramDeclaration, E> {
+        Ok(ProgramDeclaration {
             type_name: node.type_name,
-            variables: Foldable::fold(node.variables, self),
+            variables: Foldable::fold(node.variables, self)?,
             body: node.body,
-        }
+        })
     }
 
-    fn fold_variable_declaration(&mut self, node: VarDecl) -> VarDecl {
-        VarDecl {
+    fn fold_variable_declaration(&mut self, node: VarDecl) -> Result<VarDecl, E> {
+        Ok(VarDecl {
             name: node.name.clone(),
             var_type: node.var_type,
             qualifier: node.qualifier,
-            initializer: Foldable::fold(node.initializer, self),
+            initializer: Foldable::fold(node.initializer, self)?,
             position: node.position,
-        }
+        })
     }
 
-    fn fold_type_initializer(&mut self, node: TypeInitializer) -> TypeInitializer {
-        node
+    fn fold_type_initializer(&mut self, node: TypeInitializer) -> Result<TypeInitializer, E> {
+        Ok(node)
     }
 
-    fn fold_direct_variable(&mut self, node: DirectVariable) -> DirectVariable {
-        node
+    fn fold_direct_variable(&mut self, node: DirectVariable) -> Result<DirectVariable, E> {
+        Ok(node)
     }
 }
 
 impl Foldable for LibraryElement {
     type Mapped = LibraryElement;
-    fn fold<F: Fold + ?Sized>(self, folder: &mut F) -> Self::Mapped {
+    fn fold<F: Fold<E> + ?Sized, E>(self, folder: &mut F) -> Result<Self::Mapped, E> {
         folder.fold_library_element_declaration(self)
     }
 }
 
 impl Foldable for VarDecl {
     type Mapped = VarDecl;
-    fn fold<F: Fold + ?Sized>(self, folder: &mut F) -> Self::Mapped {
+    fn fold<F: Fold<E> + ?Sized, E>(self, folder: &mut F) -> Result<Self::Mapped, E> {
         folder.fold_variable_declaration(self)
     }
 }
 
 impl Foldable for TypeInitializer {
     type Mapped = TypeInitializer;
-    fn fold<F: Fold + ?Sized>(self, folder: &mut F) -> Self::Mapped {
+    fn fold<F: Fold<E> + ?Sized, E>(self, folder: &mut F) -> Result<Self::Mapped, E> {
         folder.fold_type_initializer(self)
     }
 }
 
 impl Foldable for DirectVariable {
     type Mapped = DirectVariable;
-    fn fold<F: Fold + ?Sized>(self, folder: &mut F) -> Self::Mapped {
+    fn fold<F: Fold<E> + ?Sized, E>(self, folder: &mut F) -> Result<Self::Mapped, E> {
         folder.fold_direct_variable(self)
     }
 }
