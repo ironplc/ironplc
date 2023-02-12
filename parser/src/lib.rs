e7d7e555cddfcee5b767824ccedbda6d06af5381
Garret Fick <garret@ficksworkshop.com>
2023-02-12 10:44:33 -0500

    Add a preprocessor that removes comments so that comments are supported (#37)

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -3,15 +3,18 @@ extern crate ironplc_dsl as dsl;
 pub mod error;
 mod mapper;
 mod parser;
+mod preprocessor;
 
 use crate::parser::parse_library;
 use error::ParserDiagnostic;
 use ironplc_dsl::common::Library;
+use preprocessor::preprocess;
 
 #[cfg(test)]
 mod tests;
 
 /// Parse a full IEC 61131 program.
 pub fn parse_program(source: &str) -> Result<Library, ParserDiagnostic> {
-    parse_library(source).map(|elements| Library { elements })
+    let source = preprocess(source)?;
+    parse_library(&source).map(|elements| Library { elements })
 }
