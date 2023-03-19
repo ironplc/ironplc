f10967df5f3440a36a833ead04a7bd78f6c9ff72
Garret Fick <garret@ficksworkshop.com>
2023-03-18 22:21:52 -0400

    Fix command line documentation and add integration tests for the CLI (#52)
    Fix command line documentation and add integraion tests for the CLI

---
+++ plc2x/tests/cli.rs
@@ -0,0 +1,49 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::{path::PathBuf, process::Command};
+
+pub fn path_to_test_resource(name: &'static str) -> PathBuf {
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.push("resources");
+    path.push("test");
+    path.push(name);
+    path
+}
+
+#[test]
+fn check_when_not_a_file_then_err() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("ironplcc")?;
+
+    cmd.arg("check").arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("Failed opening file"));
+
+    Ok(())
+}
+
+#[test]
+fn check_when_valid_file_then_ok() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("ironplcc")?;
+
+    cmd.arg("check")
+        .arg(path_to_test_resource("first_steps.st"));
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("OK"));
+
+    Ok(())
+}
+
+#[test]
+fn check_when_syntax_error_file_then_err() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("ironplcc")?;
+
+    cmd.arg("check")
+        .arg(path_to_test_resource("first_steps_syntax_error.st"));
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("Syntax error"));
+
+    Ok(())
+}
