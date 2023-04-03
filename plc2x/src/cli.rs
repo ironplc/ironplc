0a92d93acc1c4ac61024bb2498353221960386ec
Garret Fick <garret@ficksworkshop.com>
2023-04-02 20:50:12 -0400

    Rebase to main (#64)

--- plc2x/src/cli.rs
+++ plc2x/src/cli.rs
@@ -8,17 +8,31 @@ use codespan_reporting::{
         termcolor::{ColorChoice, StandardStream},
     },
 };
-use std::{fs::File, io::Read, ops::Range, path::PathBuf};
+use std::{
+    fs::{metadata, read_dir, File},
+    io::Read,
+    ops::Range,
+    path::PathBuf,
+};
 
 use crate::stages::analyze;
 
-pub fn check(files: Vec<PathBuf>, suppress_output: bool) -> Result<(), String> {
+// Checks specified files.
+pub fn check(paths: Vec<PathBuf>, suppress_output: bool) -> Result<(), String> {
+    let mut files: Vec<PathBuf> = vec![];
+    for path in paths {
+        match enumerate_files(path) {
+            Ok(mut paths) => files.append(&mut paths),
+            Err(err) => return Err(err),
+        }
+    }
+
     for filename in files {
         let mut file = File::open(filename.clone())
             .map_err(|e| format!("Failed opening file {}. {}", filename.display(), e))?;
         let mut contents = String::new();
         file.read_to_string(&mut contents)
-            .map_err(|_| "Failed to read file")?;
+            .map_err(|err| format!("Failed to read file {}\n {}", filename.display(), err))?;
 
         let writer = StandardStream::stderr(ColorChoice::Always);
         let config = codespan_reporting::term::Config::default();
@@ -45,9 +59,34 @@ pub fn check(files: Vec<PathBuf>, suppress_output: bool) -> Result<(), String> {
     Ok(())
 }
 
+fn enumerate_files(path: PathBuf) -> Result<Vec<PathBuf>, String> {
+    let metadata = metadata(path.as_path()).map_err(|e| e.to_string())?;
+    if metadata.is_dir() {
+        let paths = read_dir(path).map_err(|e| e.to_string())?;
+        let paths: Vec<PathBuf> = paths
+            .into_iter()
+            .filter_map(|entry| match entry {
+                Ok(entry) => Some(entry.path()),
+                Err(_) => None,
+            })
+            .collect();
+        return Ok(paths);
+    }
+    if metadata.is_file() {
+        return Ok(vec![path]);
+    }
+    if metadata.is_symlink() {
+        panic!("Sorry. Symlinks are not supported.")
+    }
+    Ok(vec![])
+}
+
 fn map_label(label: ironplc_dsl::diagnostic::Label, style: LabelStyle) -> Label<usize> {
     let range = match label.location {
-        ironplc_dsl::diagnostic::Location::QualifiedPosition(_pos) => Range { start: 0, end: 0 },
+        ironplc_dsl::diagnostic::Location::QualifiedPosition(pos) => Range {
+            start: pos.offset,
+            end: pos.offset,
+        },
         ironplc_dsl::diagnostic::Location::OffsetRange(offset) => Range {
             start: offset.start,
             end: offset.end,
