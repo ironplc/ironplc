c9a40d1105388520e0e96fecd0300859dfeebb0d
Garret Fick <garret@ficksworkshop.com>
2023-06-04 11:49:32 -0400

    Feature/publishhomebrew (#115)
    * Initial work to create homebrew tap as a publish step
    
    * Build the Homebrew tap correctly

---
+++ compiler/homebrew/Formula/ironplc.rb
@@ -0,0 +1,32 @@
+# Homebrew Formula for installing IronPLC Compiler on macOS or Linux.
+#
+# This file may be in one of two forms:
+# * a template with variables that can be filled in at build time
+# * the filled in template
+#
+# The template form has the following variables:
+# * VERSION - the bare version number, such as 1.2.3
+# * MACFILENAME - the name of the TAR.GZ file containing ironplcc, such as ironplcc-x86_64-apple-darwin.tar.gz
+# * MACSHA256 - the SHA256 of {MACFILENAME}
+# * LINUXFILENAME - the name of the TAR.GZ file containing ironplcc, such as ironplcc-x86_64-unknown-linux-musl.tar.gz
+# * LINUXSHA256 - the SHA256 of {LINUXFILENAME}
+# 
+# The formula assumes releases are from the GitHub ironplc/ironplc repository
+# and that releases are prefixed with "v".
+class IronPlcBin < Formula
+    version "${VERSION}"
+    desc "IronPLC Compiler"
+    homepage "https://www.ironplc.com"
+  
+    if OS.mac?
+        url "https://github.com/ironplc/ironplc/releases/download/v${VERSION}/${MACFILENAME}"
+        sha256 "${MACSHA256}"
+    elsif OS.linux?
+        url "https://github.com/ironplc/ironplc/releases/download/v${VERSION}/${LINUXFILENAME}"
+        sha256 "${LINUXSHA256}"
+    end
+  
+    def install
+      bin.install "ironplcc"
+    end
+  end
