1c357d8f6b0fbf6e7719aad163b983487172e168
Garret Fick <garret@garretfick.com>
2023-10-22 08:17:04 -0700

    Feature/ext (#172)
    * Remove the lsp.ts file to simplify imports
    
    * Move the extension to be in the main directory

--- integrations/vscode/src/extension.ts
+++ integrations/vscode/src/extension.ts
@@ -3,8 +3,18 @@ import * as path from 'path';
 import { existsSync } from 'fs';
 import {
 	LanguageClient,
+	LanguageClientOptions,
+	TransportKind,
+	ServerOptions,
 } from 'vscode-languageclient/node';
-import { createClient } from './lsp';
+
+const VERBOSITY = new Map<string, string[]>([
+	["ERROR", []],
+	["WARN", ["-v"]],
+	["INFO", ["-v", "-v"]],
+	["DEBUG", ["-v", "-v", "-v"]],
+	["TRACE", ["-v", "-v", "-v", "-v"]],
+]);
 
 let client: LanguageClient | undefined;
 
@@ -32,6 +42,40 @@ export function activate(context: vscode.ExtensionContext) {
 	} else {
 		console.error('Extension "ironplc" is NOT active!');
 	}
+}
+
+function createClient(compilerFilePath: string, config: vscode.WorkspaceConfiguration) {
+	const logLevel = config.get<string>('logLevel', 'ERROR');
+	const logVerbosity = VERBOSITY.get(logLevel) || [];
+
+	const args = logVerbosity.concat(['lsp']);
+	console.log('Extension "ironplc" starting with args' + args);
+
+	const application = {
+		command: compilerFilePath,
+		transport: TransportKind.stdio,
+		args: args,
+		options: {
+			env: ['RUST_LOG=lsp_server=debug']
+		}
+	};
+
+	const serverOptions: ServerOptions = application;
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [{ scheme: 'file', language: '61131-3-st' }]
+	};
+
+	// Create the language client and start the client.
+	const client = new LanguageClient(
+		'ironplc',
+		'IronPLC',
+		serverOptions,
+		clientOptions
+	);
+
+	return client;
 }
 
 function findCompiler() {
