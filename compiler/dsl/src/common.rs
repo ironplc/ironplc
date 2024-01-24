261a1c4b89cea1683ea3553c7d743058ee787359
Garret Fick <garret@garretfick.com>
2024-01-24 03:37:13 +0000

    Fix formatting

--- compiler/dsl/src/common.rs
+++ compiler/dsl/src/common.rs
@@ -567,13 +567,22 @@ pub struct Repeated {
 impl Repeated {
     pub fn recurse_visit<V: Visitor<E> + ?Sized, E>(&self, v: &mut V) -> Result<V::Value, E> {
         v.visit_integer(&self.size)?;
-        self.init.as_ref().as_ref().map_or_else(|| Ok(V::Value::default()), |val| v.visit_array_initial_element_kind(&val))
+        self.init.as_ref().as_ref().map_or_else(
+            || Ok(V::Value::default()),
+            |val| v.visit_array_initial_element_kind(val),
+        )
     }
 }
 
 impl Repeated {
     pub fn recurse_fold<F: Fold<E> + ?Sized, E>(self, f: &mut F) -> Result<Repeated, E> {
-        let init = self.init.as_mut().as_mut().map(|x| f.fold_array_initial_element_kind(x)).transpose()?;
+        let init = self
+            .init
+            .as_ref()
+            .as_ref()
+            .map(|x| f.fold_array_initial_element_kind(x.clone()))
+            .transpose()?;
+
         Ok(Repeated {
             size: f.fold_integer(self.size)?,
             init: Box::new(init),
