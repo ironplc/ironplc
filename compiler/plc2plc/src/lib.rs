92f429dbcb5077980b1b33e46ff505720fe4fa79
Garret Fick <garret@garretfick.com>
2024-03-03 05:16:24 +0000

    Render more of a library to a string

--- compiler/plc2plc/src/lib.rs
+++ compiler/plc2plc/src/lib.rs
@@ -1,7 +1,10 @@
+extern crate ironplc_dsl as dsl;
+
 use ironplc_dsl::{common::Library, diagnostic::Diagnostic};
 use renderer::apply;
 
 mod renderer;
+mod tests;
 
 pub fn write(lib: &Library) -> Result<String, Vec<Diagnostic>> {
     apply(lib)
--- compiler/plc2plc/src/renderer.rs
+++ compiler/plc2plc/src/renderer.rs
@@ -30,51 +30,34 @@ impl LibraryRenderer {
     fn write(&mut self, val: &str) {
         self.buffer.push_str(val);
     }
+
+    fn write_char(&mut self, val: char) {
+        self.buffer.push(val);
+    }
+
+    fn write_ws(&mut self, val: &str) {
+        if !self.buffer.ends_with(' ') {
+            self.buffer.push(' ');
+        }
+        self.buffer.push_str(val);
+    }
+
+    fn newline(&mut self) {
+        self.buffer.push('\n');
+    }
 }
 
 impl Visitor<Diagnostic> for LibraryRenderer {
     type Value = ();
 
     fn visit_id(&mut self, node: &Id) -> Result<Self::Value, Diagnostic> {
-        self.write(node.original.as_str());
+        // TODO this is the wrong case
+        self.write_ws(node.original().as_str());
         Ok(())
     }
 
-    fn visit_var_decl(&mut self, node: &VarDecl) -> Result<Self::Value, Diagnostic> {
-        let var_type = match node.var_type {
-            VariableType::Var => "VAR",
-            VariableType::VarTemp => "VAR_TEMP",
-            VariableType::Input => "VAR_INPUT",
-            VariableType::Output => "VAR_OUTPUT",
-            VariableType::InOut => "VAR_IN_OUT",
-            VariableType::External => "VAR_EXTERNAL",
-            VariableType::Global => "VAR_GLOBAL",
-            VariableType::Access => "VAR_ACCESS",
-        };
-        self.write(var_type);
-
-        match node.qualifier {
-            DeclarationQualifier::Unspecified => {}
-            DeclarationQualifier::Constant => self.write("CONSTANT"),
-            DeclarationQualifier::Retain => self.write("RETAIN"),
-            DeclarationQualifier::NonRetain => self.write("NON_RETAIN"),
-        }
-
-        self.write("\n");
-
-        match &node.identifier {
-            VariableIdentifier::Symbol(id) => {
-                id.recurse_visit(self)?;
-            }
-            VariableIdentifier::Direct(direct) => {
-                direct.recurse_visit(self)?;
-            }
-        }
-
-        node.recurse_visit(self)?;
-        self.write(";\n");
-
-        self.write("END_VAR\n");
+    fn visit_integer(&mut self, node: &Integer) -> Result<Self::Value,Diagnostic> {
+        self.write_ws(node.value.to_string().as_str());
         Ok(())
     }
 
@@ -94,11 +77,13 @@ impl Visitor<Diagnostic> for LibraryRenderer {
         &mut self,
         node: &DataTypeDeclarationKind,
     ) -> Result<Self::Value, Diagnostic> {
-        self.write("TYPE\n");
+        self.write_ws("TYPE");
+        self.newline();
 
         node.recurse_visit(self)?;
 
-        self.write("END_TYPE\n");
+        self.write_ws("END_TYPE");
+        self.newline();
         Ok(())
     }
 
@@ -119,5 +104,163 @@ impl Visitor<Diagnostic> for LibraryRenderer {
         self.visit_signed_integer(&node.start)?;
         self.write("..");
         self.visit_signed_integer(&node.end)
+    }
+
+    
+    // 2.4.3
+    fn visit_var_decl(&mut self, node: &VarDecl) -> Result<Self::Value, Diagnostic> {
+        self.newline();
+
+        let var_type = match node.var_type {
+            VariableType::Var => "VAR",
+            VariableType::VarTemp => "VAR_TEMP",
+            VariableType::Input => "VAR_INPUT",
+            VariableType::Output => "VAR_OUTPUT",
+            VariableType::InOut => "VAR_IN_OUT",
+            VariableType::External => "VAR_EXTERNAL",
+            VariableType::Global => "VAR_GLOBAL",
+            VariableType::Access => "VAR_ACCESS",
+        };
+        self.write_ws(var_type);
+
+        match node.qualifier {
+            DeclarationQualifier::Unspecified => {}
+            DeclarationQualifier::Constant => self.write_ws("CONSTANT"),
+            DeclarationQualifier::Retain => self.write_ws("RETAIN"),
+            DeclarationQualifier::NonRetain => self.write_ws("NON_RETAIN"),
+        }
+
+        self.newline();
+
+        match &node.identifier {
+            VariableIdentifier::Symbol(id) => {
+                self.visit_id(id)?;
+            }
+            VariableIdentifier::Direct(direct) => {
+                self.visit_direct_variable_identifier(direct)?;
+            }
+        }
+
+        self.write_ws(":");
+        self.visit_initial_value_assignment_kind(&node.initializer)?;
+
+        self.write(";");
+        self.newline();
+
+        self.write_ws("END_VAR");
+        self.newline();
+        Ok(())
+    }
+
+    // 2.4.3.1
+    fn visit_address_assignment(&mut self, node: &AddressAssignment) -> Result<Self::Value,Diagnostic> {
+        self.write_char('%');
+        
+        let loc = match &node.location {
+            LocationPrefix::I => 'I',
+            LocationPrefix::Q => 'Q',
+            LocationPrefix::M => 'M',
+        };
+        self.write_char(loc);
+
+        let size = match &node.size {
+            // TODO
+            SizePrefix::Unspecified => ' ',
+            SizePrefix::Nil => todo!(),
+            SizePrefix::X => 'X',
+            SizePrefix::B => 'B',
+            SizePrefix::W => 'W',
+            SizePrefix::D => 'D',
+            SizePrefix::L => 'L',
+        };
+        self.write_char(size);
+
+        for idx in &node.address {
+            self.write(idx.to_string().as_str());
+        }
+
+        Ok(())
+    }
+
+
+    // 2.4.3.2
+    fn visit_simple_initializer(&mut self, node: &SimpleInitializer) -> Result<Self::Value, Diagnostic> {
+        self.write_ws(&node.type_name.to_string());
+
+        match &node.initial_value {
+            Some(iv) => {
+                self.write_ws(":=");
+                self.visit_constant_kind(iv)?;
+            },
+            None => {},
+        }
+
+        Ok(())
+    }
+
+    fn visit_direct_variable_identifier(&mut self, node: &DirectVariableIdentifier) -> Result<Self::Value, Diagnostic> {
+        match &node.name {
+            Some(name) =>  { self.visit_id(name)? },
+            None => {},
+        }
+        
+        self.write_ws("AT");
+
+        self.visit_address_assignment(&node.address_assignment)?;
+
+        Ok(())
+    }
+
+    // 2.5.1
+    fn visit_function_declaration(
+        &mut self,
+        _node: &FunctionDeclaration
+    ) -> Result<Self::Value, Diagnostic> {
+        self.write_ws("FUNCTION");
+
+        self.write_ws("END_FUNCTION");
+        self.newline();
+        Ok(())
+    }
+
+    // 2.5.1
+    fn visit_function_block_declaration(
+        &mut self,
+        node: &FunctionBlockDeclaration,
+    ) -> Result<Self::Value, Diagnostic> {
+        self.write_ws("FUNCTION_BLOCK");
+
+        node.name.recurse_visit(self)?;
+        self.newline();
+
+        for var in node.variables.iter() {
+            var.recurse_visit(self)?;
+        }
+
+        node.body.recurse_visit(self)?;
+
+        self.write_ws("END_FUNCTION_BLOCK");
+        self.newline();
+        Ok(())
+    }
+
+    fn visit_program_declaration(
+        &mut self,
+        node: &ProgramDeclaration,
+    ) -> Result<Self::Value, Diagnostic> {
+        self.write_ws("PROGRAM");
+
+        self.visit_id(&node.type_name)?;
+        self.newline();
+
+        for var in node.variables.iter() {
+            self.visit_var_decl(var)?;
+        }
+
+        node.body.recurse_visit(self)?;
+
+        self.write_ws("END_PROGRAM");
+        self.newline();
+        Ok(())
     }
 }
