dd4dace39a2232ebb150b4aa6a8efbc6bb03b345
Garret Fick <garret@garretfick.com>
2024-04-27 17:00:48 -0400

    Refactor tokens so that they will work with the parser (#241)

--- compiler/parser/src/lib.rs
+++ compiler/parser/src/lib.rs
@@ -4,21 +4,18 @@
 extern crate ironplc_dsl as dsl;
 
 mod keyword;
+mod lexer;
 mod mapper;
 mod parser;
 mod preprocessor;
 
 use crate::parser::parse_library;
-use dsl::{
-    core::FileId,
-    diagnostic::{Diagnostic, Label},
-};
+use dsl::{core::FileId, diagnostic::Diagnostic};
 use ironplc_dsl::common::Library;
-use logos::Logos;
+use lexer::tokenize;
 use preprocessor::preprocess;
-use token::TokenType;
+use token::Token;
 
-pub mod lexer;
 #[cfg(test)]
 mod tests;
 pub mod token;
@@ -28,28 +25,8 @@ pub mod token;
 /// Returns a list of tokens and a list of diagnostics. This does not return a result
 /// because we usually continue with parsing even if there are token errors because
 /// that will give the context of what was wrong in the location with the error.
-pub fn tokenize_program(source: &str, file_id: &FileId) -> (Vec<TokenType>, Vec<Diagnostic>) {
-    let mut tokens = Vec::new();
-    let mut diagnostics = Vec::new();
-    let mut lexer = TokenType::lexer(source);
-
-    while let Some(token) = lexer.next() {
-        match token {
-            Ok(tok) => {
-                tokens.push(tok);
-            }
-            Err(_) => {
-                let span = lexer.span();
-                println!("{:?}", span);
-                diagnostics.push(Diagnostic::problem(
-                    ironplc_problems::Problem::UnexpectedToken,
-                    Label::offset(file_id.clone(), span, "Range of unexpected token"),
-                ))
-            }
-        }
-    }
-
-    (tokens, diagnostics)
+pub fn tokenize_program(source: &str, file_id: &FileId) -> (Vec<Token>, Vec<Diagnostic>) {
+    tokenize(source, file_id)
 }
 
 /// Parse a full IEC 61131 program.
